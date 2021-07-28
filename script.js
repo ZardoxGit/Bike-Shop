57c57ff56982b5b650ba9ba62f129ec7bffdf7ab
ZardoxGit <sounouk@hotmail.fr>
2021-07-28 22:26:01 +0200

    burgermenu js

---
+++ script.js
@@ -0,0 +1,8 @@
+var burgerMenu = document.getElementsByClassName('fas')[0]
+var navBar = document.getElementsByClassName('nav-links')[0]
+
+burgerMenu.addEventListener('click', 
+function() {
+    navBar.style.display = 'block';
+}
+);
